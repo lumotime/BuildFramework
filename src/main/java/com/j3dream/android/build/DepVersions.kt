d5f33043c2af536b4c88c95e118a371b8fd28951
j3dream <rivkaer@qq.com>
2020-11-05 09:53:42 +0800

    chore: 增加常用的依赖环境结构

--- src/main/java/com/j3dream/android/build/DepVersions.kt
+++ src/main/java/com/j3dream/android/build/DepVersions.kt
@@ -52,14 +52,30 @@ internal object DepVersions {
 
     object AndroidX{
         const val core_ktx = "1.3.2"
+        const val concurrent_futures = "1.1.0"
+        const val arch_core = "2.1.0"
         const val appcompat = "1.2.0"
         const val constraintlayout = "2.0.2"
         const val multidex = "2.0.1"
         const val recyclerview = "1.1.0"
+        const val viewpager2 = "1.0.0"
+        const val emoji = "1.1.0"
+        const val startup = "1.0.0"
+        const val jetpack_room = "2.2.3"
+        const val jetpack_camera = "1.0.0-beta10"
+        const val jetpack_workmanager = "2.3.0"
+        const val jetpack_fragment = "1.2.5"
+        const val jetpack_lifecycle = "2.2.0"
+        const val jetpack_paging = "2.2.0"
+        const val material = "1.2.1"
+        const val exifinterface = "1.3.1"
+        const val cardview = "1.0.0"
     }
 
     object Tencent {
         const val qmui = "2.0.0-alpha10"
         const val bugly_crash = "2.8.6.0"
+        const val mmkv = "1.2.4"
+        const val x5tbs = "43939"
     }
 }
--- src/main/java/com/j3dream/android/build/Dependencies.kt
+++ src/main/java/com/j3dream/android/build/Dependencies.kt
@@ -57,15 +57,52 @@ object Dependencies {
 
     object AndroidX{
         val core_ktx = buildDependency("androidx.core:core-ktx", DepVersions.AndroidX.core_ktx)
+        // androidx 协程相关
+        val concurrent_futures = buildDependency("androidx.concurrent:concurrent-futures", DepVersions.AndroidX.concurrent_futures)
+        val concurrent_futures_ktx = buildDependency("androidx.concurrent:concurrent-futures-ktx", DepVersions.AndroidX.concurrent_futures)
+        val arch_core_runtime = buildDependency("androidx.arch.core:core-runtime", DepVersions.AndroidX.arch_core)
+        val arch_core_common = buildDependency("androidx.arch.core:core-common", DepVersions.AndroidX.arch_core)
         val appcompat = buildDependency("androidx.appcompat:appcompat", DepVersions.AndroidX.appcompat)
         val constraintlayout = buildDependency("androidx.constraintlayout:constraintlayout", DepVersions.AndroidX.constraintlayout)
+        // Android 多dex
         val multidex = buildDependency("androidx.multidex:multidex", DepVersions.AndroidX.multidex)
         val recyclerview = buildDependency("androidx.recyclerview:recyclerview", DepVersions.AndroidX.recyclerview)
+        val cardview = buildDependency("androidx.cardview:cardview", DepVersions.AndroidX.cardview)
+        val material = buildDependency("com.google.android.material:material", DepVersions.AndroidX.material)
+        // 用于提取 exif 信息
+        val exifinterface = buildDependency("androidx.exifinterface:exifinterface", DepVersions.AndroidX.exifinterface)
+        // 统一 emoji 表情包
+        val emoji = buildDependency("androidx.emoji:emoji", DepVersions.AndroidX.emoji)
+        val emoji_appcompat = buildDependency("androidx.emoji:emoji-appcompat", DepVersions.AndroidX.emoji)
+        val emoji_bundled = buildDependency("androidx.emoji:emoji-bundled", DepVersions.AndroidX.emoji)
+        // 管理应用启动
+        val startup = buildDependency("androidx.startup:startup-runtime", DepVersions.AndroidX.startup)
+        // viewpager2
+        val viewpager2 = buildDependency("androidx.viewpager2:viewpager2", DepVersions.AndroidX.viewpager2)
+        val room_runtime = buildDependency("androidx.room:room-runtime", DepVersions.AndroidX.jetpack_room)
+        val room_ktx = buildDependency("androidx.room:room-ktx", DepVersions.AndroidX.jetpack_room)
+        val room_rxjava2 = buildDependency("androidx.room:room-rxjava2", DepVersions.AndroidX.jetpack_room)
+        val room_test = buildDependency("androidx.room:room-testing", DepVersions.AndroidX.jetpack_room)
+        val camera_core = buildDependency("androidx.camera:camera-core", DepVersions.AndroidX.jetpack_camera)
+        val camera_camera2 = buildDependency("androidx.camera:camera-camera2", DepVersions.AndroidX.jetpack_camera)
+        val camera_lifecycle = buildDependency("androidx.camera:camera-lifecycle", DepVersions.AndroidX.jetpack_camera)
+        val fragment = buildDependency("androidx.fragment:fragment", DepVersions.AndroidX.jetpack_fragment)
+        val fragment_ktx = buildDependency("androidx.fragment:fragment-ktx", DepVersions.AndroidX.jetpack_fragment)
+        val fragment_debug = buildDependency("androidx.fragment:fragment-testing", DepVersions.AndroidX.jetpack_fragment)
+        val lifecycle_runtime = buildDependency("androidx.lifecycle:lifecycle-runtime-ktx", DepVersions.AndroidX.jetpack_lifecycle)
+        val lifecycle_viewmodel_savedstate = buildDependency("androidx.lifecycle:lifecycle-viewmodel-savedstate", DepVersions.AndroidX.jetpack_lifecycle)
+        val paging_runtime = buildDependency("androidx.paging:paging-runtime", DepVersions.AndroidX.jetpack_paging)
+        val paging_rxjava2 = buildDependency("androidx.paging:paging-rxjava2", DepVersions.AndroidX.jetpack_paging)
+        val work_runtime = buildDependency("androidx.work:work-runtime", DepVersions.AndroidX.jetpack_workmanager)
+        val work_ktx = buildDependency("androidx.work:work-runtime-ktx", DepVersions.AndroidX.jetpack_workmanager)
+        val work_rxjava2 = buildDependency("androidx.work:work-rxjava2", DepVersions.AndroidX.jetpack_workmanager)
     }
 
     object Tencent {
         val qmui = buildDependency("com.qmuiteam:qmui", DepVersions.Tencent.qmui)
         val bugly_crash = buildDependency("com.tencent.bugly:crashreport", DepVersions.Tencent.bugly_crash)
+        val mmkv = buildDependency("com.tencent:mmkv-static", DepVersions.Tencent.mmkv)
+        val x5tbs = buildDependency("com.tencent.tbs.tbssdk:sdk", DepVersions.Tencent.x5tbs)
     }
 
     /**
